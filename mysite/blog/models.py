25e304bfcf9cb46c27982d03db1840c2aefd42e4
Dima Dudchenko <DudchenkoDV1703@yandex.ru>
2024-04-04 18:24:50 +0300

    update PostModel author attribute

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
 
@@ -11,6 +12,9 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(get_user_model(),
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
