a7a90479cb5ca2a9ac518d0bf94d4fcefbd348ea
Dima Dudchenko <DudchenkoDV1703@yandex.ru>
2024-04-05 14:59:20 +0300

    add def post_share

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,10 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 
 from .models import Post
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -40,3 +42,31 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    # Получить пост по id
+    post = get_object_or_404(
+        Post, id=post_id,
+        status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Форма передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Форма прошла валидацию успешно
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'my_account@gmail.com',
+                      [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
