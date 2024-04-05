341ab72c576a8501075dea28390afceca75c7d44
Dima Dudchenko <DudchenkoDV1703@yandex.ru>
2024-04-05 12:50:21 +0300

    fix urlpatterns

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,7 +8,7 @@ app_name = 'blog'
 urlpatterns = []
 
 # Пути поста
-urlpatterns = [
+urlpatterns += [
     path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
 ]
