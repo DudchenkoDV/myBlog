7df121fc0444c4b4acb01986aa80b5beb596d563
Dima Dudchenko <DudchenkoDV1703@yandex.ru>
2024-04-05 17:10:19 +0300

    add model Comment

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -11,7 +11,7 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
-    """Модель поста"""
+    """Модель поста."""
 
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -50,3 +50,25 @@ class Post(models.Model):
                              self.publish.month,
                              self.publish.day,
                              self.slug])
+
+
+class Comment(models.Model):
+    """Модель комментария."""
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
