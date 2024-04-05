b019feef346f4e0cb9ab53ef4bd6c9bf2ac60eb5
Dima Dudchenko <DudchenkoDV1703@yandex.ru>
2024-04-05 17:24:51 +0300

    add def post_comment

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,9 +2,10 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
+from django.views.decorators.http import require_POST
 
-from .models import Post
-from .forms import EmailPostForm
+from blog.models import Comment, Post
+from blog.forms import CommentForm, EmailPostForm
 
 
 def post_list(request):
@@ -70,3 +71,24 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
                                                     'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post, id=post_id, status=Post.Status.PUBLISHED
+        )
+    comment = None
+    # Комментарий был отправлен
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Создать объект комментария не сохраняя в бд.
+        comment = form.save(commit=False)
+        # Установить значение поста.
+        comment.post = post
+        # Сохранить коммент в бд.
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                           {'post': post,
+                            'form': form,
+                            'comment': comment})
