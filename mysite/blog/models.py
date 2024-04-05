b862e79c384fff0e3f69902188b413220362592f
Dima Dudchenko <DudchenkoDV1703@yandex.ru>
2024-04-05 12:04:53 +0300

    add castom PublishedManager for model Post

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,12 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                      .filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     """Модель поста"""
 
@@ -22,6 +28,10 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+
+    objects = models.Manager()  # Стандартный модельный менеджер.
+    # Менеджер для работы с опубликованными постами.
+    published = PublishedManager()
 
     class Meta:
         ordering = ['publish']
