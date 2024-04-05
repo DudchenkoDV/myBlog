d2061239619a123e3af831187ae2d3c99e7a0a9e
Dima Dudchenko <DudchenkoDV1703@yandex.ru>
2024-04-05 14:40:55 +0300

    update url for post_list

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -9,7 +9,8 @@ urlpatterns = []
 
 # Пути поста
 urlpatterns += [
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail, name='post_detail'),
 ]
