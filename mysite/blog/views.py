e408236ccfbb5a681f14dec38372ff38077cb62a
Dima Dudchenko <DudchenkoDV1703@yandex.ru>
2024-04-06 19:13:15 +0300

    update def post_detail for similar_posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,11 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
 
-from blog.models import Comment, Post
+from blog.models import Post
 from blog.forms import CommentForm, EmailPostForm
 from taggit.models import Tag
 
@@ -45,10 +46,19 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Форма для комментирования пользователями
     form = CommentForm()
-    return render(request, 'blog/post/detail.html',
+    # Список схожих постов(одинаковые теги)
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+          same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request,
+                  'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
