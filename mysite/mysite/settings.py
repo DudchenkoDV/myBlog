d819860f90093dd8b92e9a963c111aa7c5f89721
Dima Dudchenko <DudchenkoDV1703@yandex.ru>
2024-04-08 17:41:57 +0300

    new db postgre

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "taggit",
     "django.contrib.sites",
     "django.contrib.sitemaps",
+    "django.contrib.postgres",
 ]
 
 MIDDLEWARE = [
@@ -66,8 +67,10 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blog",
+        'USER': 'blog',
+        'PASSWORD': 'dima1703'
     }
 }
 
